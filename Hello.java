5d4fd3aa8e031854f8970468c4432c24315b7370
Rishikesh <kotlarishikesh27@gmail.com>
2024-10-02 17:22:05 +0530

    hello java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+class Hello {
+    public static void main(String a[]) 
+    {
+        System.out.print("Hello world");
+        System.out.println(2+3);
+        //double number = 2;
+        //char c = 'a';
+        boolean value = true;
+        if()
+        {
+            
+        }
+        System.out.println(value);
+    }
+}
